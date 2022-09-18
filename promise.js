4a596f66559b489f646ee035e892460932c64e04
dwihandoko-studio <dwi.handoko0511@gmail.com>
2022-09-19 01:01:30 +0700

    -help

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  return new Promise(async (resolve) => {
+    let hasil = 0;
+    await Promise.allSettled([promiseTheaterIXX(), promiseTheaterVGC()]).then(
+      (results) =>
+        results.forEach((result) =>
+          result.value.forEach((val) => {
+            if (val.hasil == emosi) {
+              hasil++;
+            }
+          })
+        )
+    );
+    resolve(hasil);
+  });
+};
 
 module.exports = {
   promiseOutput,
